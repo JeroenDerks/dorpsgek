fea3f69654285fa53cb3a96e274bcdd7cde898c4
JeroenDerks <jeroen.derks@lau.do>
2023-02-04 12:04:04 +0100

    feat: update selected towns

--- data/chosenVillages.ts
+++ data/chosenVillages.ts
@@ -1,6 +1,17 @@
-const kleineDorpen = ['2865', '5853', '6655'];
+// Woudenberg	3931
+// Werkendam	4251
+// Bovenkarspel 1611
+// Ruurlo	7261
+// Heythuysen	6093
+// Oldebroek	8096
+// Lewedorp	4456
+// Siebengewald	5853
+// Puiflijk	6655
+// Ulrum	9971
+
+const groteDorpen = ['1611', '4251', '3931'];
 const middelDorpen = ['7261', '6093', '8096'];
-const groteDorpen = ['1611', '4251', '3931', '9971'];
+const kleineDorpen = ['4456', '5853', '6655', '9971'];
 
 export const chosenVillages = [
   ...kleineDorpen,
