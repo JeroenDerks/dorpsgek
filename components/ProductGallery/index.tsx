a09c825dd1eff8193fd33a05d2f19c1b5036a3fe
JeroenDerks <jeroen.derks@lau.do>
2023-03-23 08:37:30 +0100

    feat: reinstate next image

--- components/ProductGallery/index.tsx
+++ components/ProductGallery/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
 import { Box, styled } from '@mui/material';
@@ -13,6 +14,9 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay';
 
+import ImageBack from '../../public/product/back.jpg';
+import { getImagesPerCity } from './images';
+
 const SwiperButtonLeft = styled(Button)({
   padding: '4px 16px',
   width: '100%',
@@ -24,9 +28,8 @@ const SwiperButtonRight = styled(SwiperButtonLeft)({
   justifyContent: 'flex-end'
 });
 
-const Image = styled('img')(({ theme }) => ({
+const ImageContainer = styled('div')(({ theme }) => ({
   width: '100%',
-  objectFit: 'cover',
   minHeight: 400,
   background: '#f0f0f0',
 
@@ -36,11 +39,13 @@ const Image = styled('img')(({ theme }) => ({
 }));
 
 export const ProductGallery = ({ town }: { town: TownData }) => {
+  const currCityImages = getImagesPerCity(town.zipCodes[0]);
+
   const images = [
-    { src: `/product/${town.zipCodes[0]}_model.jpg`, alt: 'model' },
-    { src: `/product/${town.zipCodes[0]}_front.jpg`, alt: 'front' },
-    { src: `/product/back.jpg`, alt: 'back' },
-    { src: `/product/${town.zipCodes[0]}_closeup.jpg`, alt: 'closeup' }
+    { src: currCityImages.model, alt: 'model' },
+    { src: currCityImages.front, alt: 'front' },
+    { src: ImageBack, alt: 'back' },
+    { src: currCityImages.closeUp, alt: 'closeup' }
   ];
 
   return (
@@ -57,7 +62,16 @@ export const ProductGallery = ({ town }: { town: TownData }) => {
       >
         {images.map(({ src, alt }, index) => (
           <SwiperSlide key={index}>
-            <Image src={src} alt={`${town.zipCodes[0]} hoodie ${alt}`} />
+            <ImageContainer>
+              <Image
+                src={src}
+                objectFit="cover"
+                layout="fill"
+                placeholder="blur"
+                priority={index === 0}
+                alt={`${town.zipCodes[0]} hoodie ${alt}`}
+              />
+            </ImageContainer>
           </SwiperSlide>
         ))}
       </Swiper>
