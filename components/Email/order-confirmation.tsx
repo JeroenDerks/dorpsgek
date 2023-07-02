147c982a4d0e835d819ff94a70019d04f2c7c18e
JeroenDerks <jeroen.derks@lau.do>
2023-07-02 12:44:50 +0200

    feat: add email

--- components/Email/order-confirmation.tsx
+++ components/Email/order-confirmation.tsx
@@ -1,22 +1,132 @@
-import { Button } from '@react-email/button';
-import { Head } from '@react-email/head';
-import { Html } from '@react-email/html';
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Text
+} from '@react-email/components';
 import * as React from 'react';
+import {
+  adressTitle,
+  container,
+  footer,
+  html,
+  main,
+  message,
+  paddingY,
+  paragraph,
+  top
+} from './styled';
 
-export default function Email() {
-  return (
-    <Html lang="nl" dir="ltr">
-      <Head>
-        <title>Het feest kan beginnen want je bestelling is binnen.</title>
-      </Head>
-      <Button
-        pX={20}
-        pY={12}
-        href="https://example.com"
-        style={{ background: '#000', color: '#fff' }}
-      >
-        Hurray
-      </Button>
-    </Html>
-  );
-}
+const baseUrl = 'https://www.mndorp.nl';
+
+export const NikeReceiptEmail = ({
+  addressLine1,
+  addressLine2,
+  city,
+  country,
+  customerName,
+  postalCode,
+  orderDate,
+  orderId,
+  size,
+  state,
+  townColor,
+  townName,
+  zipCode = '1611'
+}: EmailOrderProps) => (
+  <Html style={html}>
+    <Head />
+    <Preview>Get your order summary, estimated delivery date and more</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Section style={top.container}>
+          <Section style={{ ...top.coloredLine, backgroundColor: townColor }} />
+        </Section>
+        <Section style={message}>
+          <Heading style={global.heading}>Aan de slag.</Heading>
+          <Text style={global.text}>Je bestelling is ontvangen</Text>
+          <Text style={{ ...global.text, marginTop: 24 }}>
+            Houd er rekening mee dat elk item apart bedrukt moet worden. Daarom
+            is de bezorgtijd zo'n 3 tot 5 dagen. Alvast bedankt voor je geduld,
+            en veel plezier met je aankoop.
+          </Text>
+        </Section>
+        <Hr style={global.hr} />
+        <Section style={global.defaultPadding}>
+          <Text style={adressTitle}>Bestelling voor: {customerName}</Text>
+          <Text style={{ ...global.text, fontSize: 14 }}>
+            {addressLine1} {addressLine2 && addressLine2 + ' '}
+            {postalCode} {city} {state && state + ' '}
+            {country}
+          </Text>
+        </Section>
+        <Hr style={global.hr} />
+        <Section style={{ padding: '32px 16px' }}>
+          <Row>
+            <Column>
+              <Img
+                src={`${baseUrl}/product/${zipCode}_model.jpg`}
+                alt={`${townName} hoodie (${zipCode}) in maat ${size}`}
+                style={{ float: 'left' }}
+                width="260px"
+              />
+            </Column>
+            <Column style={{ verticalAlign: 'top', paddingLeft: '12px' }}>
+              <Text style={{ ...paragraph, fontWeight: '500' }}>
+                {townName} hoodie - {zipCode}
+              </Text>
+              <Text style={global.text}>Size {size}</Text>
+            </Column>
+          </Row>
+        </Section>
+        <Hr style={global.hr} />
+        <Section style={global.defaultPadding}>
+          <Row style={{ display: 'inline-flex', marginBottom: 40 }}>
+            <Column style={{ width: '170px' }}>
+              <Text style={global.paragraphWithBold}>Order nummer</Text>
+              <Text>{orderId}</Text>
+            </Column>
+            <Column>
+              <Text style={global.paragraphWithBold}>Order datum</Text>
+              <Text>{orderDate}</Text>
+            </Column>
+          </Row>
+        </Section>
+        <Hr style={{ ...global.hr, marginTop: '12px' }} />
+        <Section style={paddingY}>
+          <Text style={{ ...footer.text, paddingTop: 30, paddingBottom: 30 }}>
+            Mocht je vragen hebben, stuur dan een email.
+          </Text>
+          <Text style={footer.text}>© 2023 mndorp</Text>
+        </Section>
+      </Container>
+    </Body>
+  </Html>
+);
+
+export default NikeReceiptEmail;
+
+type EmailOrderProps = {
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  customerName: string;
+  country: string;
+  orderId: string;
+  postalCode: string;
+  price: string;
+  size: string;
+  state: string;
+  townColor: string;
+  townName: string;
+  zipCode: string;
+  orderDate: string;
+};
---
+++ components/Email/styled.ts
@@ -0,0 +1,104 @@
+export const paddingX = {
+  paddingLeft: '40px',
+  paddingRight: '40px'
+};
+
+export const paddingY = {
+  paddingTop: '22px',
+  paddingBottom: '22px'
+};
+
+export const paragraph = {
+  margin: '0',
+  lineHeight: '2'
+};
+
+export const global = {
+  paddingX,
+  paddingY,
+  defaultPadding: {
+    ...paddingX,
+    ...paddingY
+  },
+  paragraphWithBold: { ...paragraph, fontWeight: 'bold' },
+  heading: {
+    fontSize: '32px',
+    lineHeight: '1.3',
+    fontWeight: '700',
+    textAlign: 'center',
+    letterSpacing: '-1px'
+  } as React.CSSProperties,
+  text: {
+    ...paragraph,
+    color: '#747474',
+    fontWeight: '500'
+  },
+  button: {
+    border: '1px solid #929292',
+    fontSize: '16px',
+    textDecoration: 'none',
+    padding: '10px 0px',
+    width: '220px',
+    display: 'block',
+    textAlign: 'center',
+    fontWeight: 500,
+    color: '#000'
+  } as React.CSSProperties,
+  hr: {
+    borderColor: '#E5E5E5',
+    margin: '0'
+  }
+};
+
+export const main = {
+  backgroundColor: '#eaeaec',
+  padding: '16px 0',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
+};
+
+export const container = {
+  margin: '10px auto',
+  width: '600px',
+  border: '1px solid #E5E5E5',
+  padding: '16px',
+  background: '#ffffff'
+};
+
+export const top = {
+  container: {
+    padding: '32px 0px',
+    backgroundColor: '#000'
+  },
+  coloredLine: {
+    height: '8px'
+  }
+};
+
+export const message = {
+  padding: '40px 74px',
+  textAlign: 'center'
+} as React.CSSProperties;
+
+export const adressTitle = {
+  ...paragraph,
+  fontSize: '15px',
+  fontWeight: 'bold'
+};
+
+export const footer = {
+  policy: {
+    width: '166px',
+    margin: 'auto'
+  },
+  text: {
+    margin: '0',
+    color: '#AFAFAF',
+    fontSize: '13px',
+    textAlign: 'center'
+  } as React.CSSProperties
+};
+
+export const html = {
+  background: '#ffffff'
+};
